0925b485f68e6386f77f7e40a4dc903fc9c84fe2
Olzhas Akhmetkhan <iam@olzhas.kz>
2024-06-23 13:51:29 +0500

    added protobuf support

--- lua/community.lua
+++ lua/community.lua
@@ -6,6 +6,7 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.proto" },
   -- import/override with your plugins folder
   { import = "astrocommunity.completion.copilot-lua-cmp" },
   { -- further customize the options set by the community
